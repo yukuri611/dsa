222ef39c2e300819dbdd2b6577e327dcca63c830
yukuri611 <yukiiw611@gmail.com>
2025-06-15 21:51:56 +0900

    6/15

--- atcoder/aribon/ATC_001_A.cpp
+++ atcoder/aribon/ATC_001_A.cpp
@@ -3,20 +3,26 @@
 using namespace std;
 
 bool dfs(int i, int j, vector<vector<char>> &grid);
-int main() {
+int main()
+{
   int H, W;
   cin >> H >> W;
   vector<vector<char>> grid(H, vector<char>(W));
-  for(int i = 0; i < H; i++) {
-    for (int j = 0; j < W; j++) {
+  for (int i = 0; i < H; i++)
+  {
+    for (int j = 0; j < W; j++)
+    {
       cin >> grid[i][j];
     }
   }
 
   int s_i, s_j;
-  for (int i = 0; i < H; i++) {
-    for (int j = 0; j < W; j++) {
-      if (grid[i][j] == 's') {
+  for (int i = 0; i < H; i++)
+  {
+    for (int j = 0; j < W; j++)
+    {
+      if (grid[i][j] == 's')
+      {
         s_i = i;
         s_j = j;
       }
@@ -24,33 +30,42 @@ int main() {
   }
 
   bool res = dfs(s_i, s_j, grid);
-  if (res) {
+  if (res)
+  {
     cout << "Yes" << endl;
   }
-  else {
+  else
+  {
     cout << "No" << endl;
   }
   return 0;
 }
 
-bool dfs(int i, int j, vector<vector<char>> &grid) {
-  if (grid[i][j] == 'g') {
+bool dfs(int i, int j, vector<vector<char>> &grid)
+{
+  if (grid[i][j] == 'g')
+  {
     return true;
   }
 
-  pair<int, int> directions[4] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+  pair<int, int> directions[4] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
   bool res = false;
-  for (pair<int, int> direction: directions) {
-    if (i+direction.first < 0 or i+direction.first >= grid.size() or j+direction.second < 0 or j+direction.second >= grid[0].size() or grid[i][j] == '#' or grid[i][j] == 'T') {
+  for (pair<int, int> direction : directions)
+  {
+    int i_next = i + direction.first;
+    int j_next = j + direction.second;
+    if (i_next < 0 or i_next >= grid.size() or j_next < 0 or j_next >= grid[0].size() or grid[i_next][j_next] == '#' or grid[i_next][j_next] == 'T')
+    {
       continue;
     }
-    char temp = grid[i+direction.first][j+direction.second];
-    grid[i+direction.first][j+direction.second] = 'T';
-    if (dfs(i + direction.first, j + direction.second, grid)){
-      grid[i+direction.first][j+direction.second] = temp;
+    char temp = grid[i][j];
+    grid[i][j] = 'T';
+    if (dfs(i_next, j_next, grid))
+    {
+      grid[i_next][j_next] = temp;
       return true;
     }
-    grid[i+direction.first][j+direction.second] = temp;
+    grid[i + direction.first][j + direction.second] = temp;
   }
   return false;
 }
